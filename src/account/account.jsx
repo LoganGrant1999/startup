265804542823fe76f47eeefa9fef817364ae880e
Logan Gibbons <lgg25@byu.edu>
2024-11-09 19:55:39 -0700

    Changes

--- src/account/account.jsx
+++ src/account/account.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import './profile.css';
 
 export function Account() {
   return (
     <main>
-      <div>Account page displayed here</div>
+
+      <p className="streak">Your current prediction streak is at 4!</p>
+      <p><span className="email">Account Email:</span> logan@fanvote.click</p>
+
+      <h3>Voter History:</h3>
+
+      {/* Placeholder for database data of user voting history */}
+      <ul>
+
+        <li className="list1">
+          You correctly predicted the Saints would beat the Cowboys!
+        </li>
+
+        <li className="list2">
+          You correctly predicted the Gamecocks would beat the Wildcats!
+        </li>
+
+        <li className="list3">
+          You correctly predicted the Braves would beat the Royals!
+        </li>
+
+        <li className="list4">
+          You correctly predicted the Hurricanes would beat the Hokies!
+        </li>
+
+        <li className="list5">
+          You correctly predicted the Saints would beat the Cowboys!
+        </li>
+
+        <li className="list6">
+          You incorrectly predicted the Tigers would beat the Irish!
+        </li>
+
+      </ul>
+
+      <br /><br />
+
+      <div className="image-box" id="image">
+        <img src="sports-banner.jpg" alt="banner of multiple sports being played" />
+      </div>
+
+      <a className="log-out" href="index.html">Log Out</a>
     </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import './login.css';
 
 export function Login() {
   return (
-    <main>
-      <div>Login page displayed here</div>
-    </main>
+    <main class="container-fluid text-center">
+
+    <div class="login">
+      <div>
+        <h1>Welcome to FanVote</h1>
+      </div>
+      <form method="get" action="predictions.html">
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">@</span>
+          <input class="form-control" type="text" placeholder="your@email.com" />
+        </div>
+
+        <div class="input-group mb-3">
+          <span class="input-group-text">🔒</span>
+          <input class="form-control" type="password" placeholder="password" />
+        </div>
+
+        <button type="submit" className="btn btn-primary me-2" id="login-button">Login</button>
+        <button type="submit" className="btn btn-secondary" id="create-button">Create</button>
+
+
+      </form>
+    </div>
+
+    <p>
+      FanVote brings the excitement of live sports to your fingertips by letting you vote in real-time on who you think will win upcoming and ongoing games.
+      Whether it's soccer, basketball, or any other major sport, users can see live updates, engage in friendly competition with others, and track the accuracy
+      of their predictions. FanVote transforms sports watching into an interactive experience, making it fun to see how your votes stack up against your friends
+      and the rest of the world.
+    </p>
+
+  </main>
   );
 }
 
