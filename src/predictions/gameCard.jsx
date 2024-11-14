76059aad06ac20f834547c69b53b9817ba4d1484
Logan Gibbons <lgg25@byu.edu>
2024-11-14 11:36:19 -0700

    Fixed radio button

--- src/predictions/gameCard.jsx
+++ src/predictions/gameCard.jsx
@@ -7,28 +7,30 @@ export function GameCard(props) {
     const [hasVoted, setVoted] = useState(false)
 
     const updateHasVoted = () => {
-        setVoted(!hasVoted)
+        setVoted(true)
     }
 
     return(
         <div className="card">
-                    <form>
-                    <input type="radio" id="selection1" name="game1" value="BYU" />
-                    <label htmlFor="selection1">{props.team1}</label>
-                    <br /><br />
+            <form>
+                <input type="radio" id="selection1" name={`${props.team1}-vs-${props.team2}`}/>
+                <label htmlFor="selection1">{props.team1}</label>
 
-                    {/* Placeholder for calling to 3rd-party sports game API */}
-                    <img src="byu-vs-baylor.png" alt="BYU versus Baylor matchup" />
-                    <br /><br />
-                    </form>
+                <br /><br />
 
-                    <form>
-                    <input type="radio" id="selection2" name="game1" value="Baylor" />
-                    <label htmlFor="selection2">{props.team2}</label>
-                    <br /><br />
+                {/* Placeholder for calling to 3rd-party sports game API */}
+                <img src={props.upcoming_game} alt="upcoming matchup" />
+                <br /><br />
 
-                    {!hasVoted ? <input type="button" value="Submit Choice" onClick={updateHasVoted} /> : <p>You've voted</p>}
+                <input type="radio" id="selection2" name={`${props.team1}-vs-${props.team2}`}/>
 
-                    </form>
-                </div>
-)}
+                <label htmlFor="selection2">{props.team2}</label>
+                <br /><br />
+
+                {!hasVoted ? ( <input type="button" value="Submit Choice" onClick={updateHasVoted} /> ) : ( <p>Vote Sumbitted! ✅</p> )}
+
+            </form>
+        </div>
+    );
+}
+
--- src/predictions/predictions.jsx
+++ src/predictions/predictions.jsx
@@ -11,10 +11,6 @@ export function Predictions() {
         <div className="user-and-leaderboard">
           <div className="user">
             <h3>Player: {userName}</h3>
-            <div>
-              <label htmlFor="count">Prediction Streak:</label>
-              <input type="text" id="count" value="--" readOnly />
-            </div>
           </div>
 
           <div className="leaderboard" id="leaderboard-color">
@@ -30,63 +26,11 @@ export function Predictions() {
         <h1 className="upcoming">Upcoming Games</h1>
 
         <div className="container">
-          <GameCard team1="BYU" team2="Baylor"/>
-          {/* <GameCard team1="Knicks" team2="Hornets"/>
-          <GameCard team1="Bengals" team2="Panthers"/>
-          <GameCard team1= "Royals" team2="Braves"/> */}
-          <div className="card">
-            <form>
-              <input type="radio" id="selection3" name="game2" value="Knicks" />
-              <label htmlFor="selection3">Knicks</label>
-              <br /><br />
-
-              {/* Placeholder for calling to 3rd-party sports game API */}
-              <img src="knicks-vs-hornets.png" alt="Knicks versus Hornets matchup" />
-              <br /><br />
-
-              <input type="radio" id="selection4" name="game2" value="Hornets" />
-              <label htmlFor="selection4">Hornets</label>
-              <br /><br />
-
-              <input type="button" value="Submit Choice" />
-            </form>
-          </div>
-
-          <div className="card">
-            <form>
-              <input type="radio" id="selection5" name="game3" value="Bengals" />
-              <label htmlFor="selection5">Bengals</label>
-              <br /><br />
-
-              {/* Placeholder for calling to 3rd-party sports game API */}
-              <img src="bengals-vs-panthers.png" alt="Bengals versus Panthers matchup" />
-              <br /><br />
-
-              <input type="radio" id="selection6" name="game3" value="Panthers" />
-              <label htmlFor="selection6">Panthers</label>
-              <br /><br />
-
-              <input type="button" value="Submit Choice" />
-            </form>  
-          </div>
-
-          <div className="card">
-            <form>
-              <input type="radio" id="selection7" name="game4" value="Royals" />
-              <label htmlFor="selection7">Royals</label>
-              <br /><br />
-
-              {/* Placeholder for calling to 3rd-party sports game API */}
-              <img src="royals-vs-braves.png" alt="Royals versus Braves matchup" />
-              <br />
-
-              <input type="radio" id="selection8" name="game4" value="Braves" />
-              <label htmlFor="selection8">Braves</label>
-              <br /><br />
-
-              <input type="button" value="Submit Choice" />
-            </form>
-          </div>
+          {/* Placeholders for calling to 3rd-party sports game API */}
+          <GameCard team1="BYU" team2="Baylor" upcoming_game='byu-vs-baylor.png'/>
+          <GameCard team1="Knicks" team2="Hornets" upcoming_game='knicks-vs-hornets.png'/>
+          <GameCard team1="Bengals" team2="Panthers" upcoming_game='bengals-vs-panthers.png'/>
+          <GameCard team1= "Royals" team2="Braves" upcoming_game='royals-vs-braves.png'/>
         </div>
 
         <br />
