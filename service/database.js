5fcaf1f80941dc2df37170b72b59727ad1f4b8bf
Logan Gibbons <lgg25@byu.edu>
2024-11-21 20:57:22 -0700

    Start database.js

---
+++ service/database.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('fanvote');
+const userCollection = db.collection('user');
+const votesCollection = db.collection('votes');
