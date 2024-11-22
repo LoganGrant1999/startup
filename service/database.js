05c6b401ec5b32c6140f63e75f44cf5fd9ccb8d8
Logan Gibbons <lgg25@byu.edu>
2024-11-21 20:58:01 -0700

    Add login stuff to database.js

--- service/database.js
+++ service/database.js
@@ -8,3 +8,33 @@ const client = new MongoClient(url);
 const db = client.db('fanvote');
 const userCollection = db.collection('user');
 const votesCollection = db.collection('votes');
+
+(async function testConnection() {
+    await client.connect();
+    await db.command({ ping: 1 });
+  })().catch((ex) => {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  });
+  
+  function getUser(email) {
+    return userCollection.findOne({ email: email });
+  }
+  
+  function getUserByToken(token) {
+    return userCollection.findOne({ token: token });
+  }
+  
+  async function createUser(email, password) {
+    // Hash the password before we insert it into the database
+    const passwordHash = await bcrypt.hash(password, 10);
+  
+    const user = {
+      email: email,
+      password: passwordHash,
+      token: uuid.v4(),
+    };
+    await userCollection.insertOne(user);
+  
+    return user;
+  }
