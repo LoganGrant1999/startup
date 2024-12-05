72b5fb34c36eb6a76377ac4ee8163820ef8d7da1
Logan Gibbons <lgg25@byu.edu>
2024-12-05 11:44:19 -0700

    predictionNotifier fixes

--- src/predictions/predictionNotifier.js
+++ src/predictions/predictionNotifier.js
@@ -10,6 +10,8 @@ const GameEvent = {
   
       this.socket.onopen = () => console.log('WebSocket connected');
       this.socket.onclose = () => console.log('WebSocket disconnected');
+      this.socket.onmessage = (event) => this.onMessage(event); // Bind onMessage
+  
       this.handlers = [];
     }
   
@@ -17,20 +19,20 @@ const GameEvent = {
       this.handlers.push(handler);
     }
   
-
     removeHandler(handler) {
-        this.handlers = this.handlers.filter((h) => h !== handler);
-      }
-    
-      onMessage(event) {
-        const message = JSON.parse(event.data);
-        this.handlers.forEach((handler) => handler(message));
-      }
-    
-      sendMessage(type, payload) {
-        this.socket.send(JSON.stringify({ type, payload }));
-      }
+      this.handlers = this.handlers.filter((h) => h !== handler);
     }
-    
-    const predictionSocket = new predictionSocket();
-    export { predictionSocket, GameEvent };
+  
+    onMessage(event) {
+      const message = JSON.parse(event.data);
+      this.handlers.forEach((handler) => handler(message));
+    }
+  
+    sendMessage(type, payload) {
+      this.socket.send(JSON.stringify({ type, payload }));
+    }
+  }
+  
+  const predictionSocket = new predictionSocket();
+  export { predictionSocket, GameEvent };
+  
