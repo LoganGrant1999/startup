8f616187086f5d446d94b8ffd039fac839812750
Logan Gibbons <lgg25@byu.edu>
2024-12-05 12:14:25 -0700

    index

--- service/index.js
+++ service/index.js
@@ -1,51 +1,43 @@
 const cookieParser = require('cookie-parser');
 const bcrypt = require('bcrypt');
 const express = require('express');
-const uuid = require('uuid');
-const app = express();
+const { peerProxy } = require('./peerProxy');
 const DB = require('./database.js');
-const { peerProxy } = require('./peerProxy.js');
+const uuid = require('uuid');
 
+const app = express();
 const authCookieName = 'token';
 
-const port = process.argv.length > 2 ? process.argv[2]: 3000;
+const port = process.argv[2] || 3000;
 
 app.use(express.json());
-
 app.use(express.static('public'));
-
 app.use(cookieParser());
 
 app.set('trust proxy', true);
 
-var apiRouter = express.Router();
-app.use(`/api`, apiRouter);
+const apiRouter = express.Router();
+app.use('/api', apiRouter);
 
 apiRouter.post('/auth/create', async (req, res) => {
-    if (await DB.getUser(req.body.email)) {
-      res.status(409).send({ msg: 'Existing user' });
-    } else {
-      const user = await DB.createUser(req.body.email, req.body.password);
-  
-      setAuthCookie(res, user.token);
+  if (await DB.getUser(req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await DB.createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({ id: user._id });
+  }
+});
 
-      res.send({
-        id: user._id,
-      });
-    }
-  });
-  
 apiRouter.post('/auth/login', async (req, res) => {
-    const user = await DB.getUser(req.body.email);
-    if (user) {
-      if (await bcrypt.compare(req.body.password, user.password)) {
-        setAuthCookie(res, user.token);
-        res.send({ id: user._id });
-        return;
-      }
-    }
+  const user = await DB.getUser(req.body.email);
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    setAuthCookie(res, user.token);
+    res.send({ id: user._id });
+  } else {
     res.status(401).send({ msg: 'Unauthorized' });
-  });
+  }
+});
 
 apiRouter.delete('/auth/logout', (_req, res) => {
   res.clearCookie(authCookieName);
@@ -59,66 +51,56 @@ secureApiRouter.use(async (req, res, next) => {
   const authToken = req.cookies[authCookieName];
   const user = await DB.getUserByToken(authToken);
   if (user) {
+    req.user = user;
     next();
   } else {
     res.status(401).send({ msg: 'Unauthorized' });
   }
 });
 
-
 secureApiRouter.post('/votes', async (req, res) => {
-  const vote = { ...req.body, ip: req.ip };
-  const existingVote = await DB.getVoteByUserAndGame(vote.name, vote.game);
+  try {
+    const vote = { ...req.body, ip: req.ip };
+    const existingVote = await DB.getVoteByUserAndGame(vote.name, vote.game);
 
-  if (existingVote) {
-    return res.status(400).send({ msg: 'Already Voted' });
-  }
-
-  await DB.addVote(vote);
-
-  req.app.locals.wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ type: 'new-vote', payload: vote }));
+    if (existingVote) {
+      return res.status(400).send({ msg: 'Already Voted' });
     }
-  });
 
-  const votes = await DB.getUserVotes(vote.name);
-  res.send(votes);
+    await DB.addVote(vote);
+
+    app.locals.wss.clients.forEach((client) => {
+      if (client.readyState === 1) {
+        client.send(JSON.stringify({ type: 'new-vote', payload: vote }));
+      }
+    });
+
+    res.status(200).send(vote);
+  } catch (error) {
+    console.error('Error in /votes:', error);
+    res.status(500).send({ msg: 'Internal Server Error' });
+  }
 });
-
-
-
-async function getVoteByUserAndGame (name, game) {
-  return await votesCollection.findOne({ name, game });
-}
 
 secureApiRouter.get('/votes', async (req, res) => {
-  const authToken = req.cookies[authCookieName];
-  const user = await DB.getUserByToken(authToken);
   const votes = await DB.getVotes();
-  res.send(votes.filter(vote => vote.name === user.email));
-});
-
-app.use(function (err, req, res, next) {
-    res.status(500).send({ type: err.name, message: err.message });
+  res.send(votes.filter((vote) => vote.name === req.user.email));
 });
 
 app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
+  res.sendFile('index.html', { root: 'public' });
 });
 
 function setAuthCookie(res, authToken) {
-    res.cookie(authCookieName, authToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: 'strict',
-    });
+  res.cookie(authCookieName, authToken, {
+    secure: true,
+    httpOnly: true,
+    sameSite: 'strict',
+  });
 }
 
-const httpService = app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-peerProxy(httpService);
-
-  
+app.locals.wss = peerProxy(server);
